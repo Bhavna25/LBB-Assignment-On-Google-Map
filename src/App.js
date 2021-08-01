864c7d9b6b6f34f8029dc477626a161fcc0b6a4d
Bhavna25 <63115179+Bhavna25@users.noreply.github.com>
2021-08-01 11:20:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GoogleMapComponent from './component/GoogleMapComponent/GoogleMapComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleMapComponent/>
+    </div>
+  );
+}
+
+export default App;
